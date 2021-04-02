dea48bdfbe2d6d973b531d711d2a33dd974d8129
houseme <houseme@outlook.com>
2021-04-03 00:18:23 +0800

    [option] lock auto

--- idgen/YitIdHelper.go
+++ idgen/YitIdHelper.go
@@ -52,19 +52,20 @@ var idGenerator *DefaultIdGenerator
 //	return YitIDHelper{}
 //}
 
-
+// SetIdGenerator .
 func SetIdGenerator(options *IdGeneratorOptions) {
 	singletonMutex.Lock()
 	idGenerator = NewDefaultIdGenerator(options)
 	singletonMutex.Unlock()
 }
 
+// NextId .
 func NextId() uint64 {
 	if idGenerator == nil {
 		singletonMutex.Lock()
+		defer singletonMutex.Unlock()
 		options := NewIdGeneratorOptions(1)
 		idGenerator = NewDefaultIdGenerator(options)
-		singletonMutex.Unlock()
 	}
 
 	return idGenerator.NewLong()
