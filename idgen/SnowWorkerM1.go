b12d957d50892a147efd253829b72ec55c9aaa47
zhouzj <zhouzj@zhouzj.com>
2021-04-03 13:02:54 +0800

    auto commit

--- idgen/SnowWorkerM1.go
+++ idgen/SnowWorkerM1.go
@@ -1,3 +1,9 @@
+/*
+ * 版权属于：yitter(yitter@126.com)
+ * 代码编辑：guoyahao
+ * 代码修订：yitter
+ * 开源地址：https://gitee.com/yitter/idgenerator
+ */
 package idgen
 
 import (
@@ -7,15 +13,16 @@ import (
 
 // SnowWorkerM1 .
 type SnowWorkerM1 struct {
-	BaseTime                int64  //基础时间
-	WorkerId                uint16 //机器码
-	WorkerIdBitLength       byte   //机器码位长
-	SeqBitLength            byte   //自增序列数位长
-	MaxSeqNumber            uint32 //最大序列数（含）
-	MinSeqNumber            uint32 //最小序列数（含）
-	TopOverCostCount        uint32 //最大漂移次数
-	_TimestampShift         byte
-	_CurrentSeqNumber       uint32
+	BaseTime          int64  //基础时间
+	WorkerId          uint16 //机器码
+	WorkerIdBitLength byte   //机器码位长
+	SeqBitLength      byte   //自增序列数位长
+	MaxSeqNumber      uint32 //最大序列数（含）
+	MinSeqNumber      uint32 //最小序列数（含）
+	TopOverCostCount  uint32 //最大漂移次数
+	_TimestampShift   byte
+	_CurrentSeqNumber uint32
+
 	_LastTimeTick           int64
 	_TurnBackTimeTick       int64
 	_TurnBackIndex          byte
@@ -23,6 +30,7 @@ type SnowWorkerM1 struct {
 	_OverCostCountInOneTerm uint32
 	_GenCountInOneTerm      uint32
 	_TermIndex              uint32
+
 	sync.Mutex
 }
 
@@ -32,26 +40,7 @@ func NewSnowWorkerM1(options *IdGeneratorOptions) ISnowWorker {
 	var seqBitLength byte
 	var maxSeqNumber uint32
 
-	var workerId = options.WorkerId
-
-	if options.WorkerIdBitLength == 0 {
-		workerIdBitLength = 6
-	} else {
-		workerIdBitLength = options.WorkerIdBitLength
-	}
-	if options.SeqBitLength == 0 {
-		seqBitLength = 6
-	} else {
-		seqBitLength = options.SeqBitLength
-	}
-	if options.MaxSeqNumber > 0 {
-		maxSeqNumber = options.MaxSeqNumber
-	} else {
-		maxSeqNumber = (1 << seqBitLength) - 1
-	}
-	var minSeqNumber = options.MinSeqNumber
-	var topOverCostCount = options.TopOverCostCount
-
+	// 1.BaseTime
 	var baseTime int64
 	if options.BaseTime != 0 {
 		baseTime = options.BaseTime
@@ -59,50 +48,87 @@ func NewSnowWorkerM1(options *IdGeneratorOptions) ISnowWorker {
 		baseTime = 1582136402000
 	}
 
-	timestampShift := (byte)(options.WorkerIdBitLength + options.SeqBitLength)
-	currentSeqNumber := options.MinSeqNumber
+	// 2.WorkerIdBitLength
+	if options.WorkerIdBitLength == 0 {
+		workerIdBitLength = 6
+	} else {
+		workerIdBitLength = options.WorkerIdBitLength
+	}
+
+	// 3.WorkerId
+	var workerId = options.WorkerId
+
+	// 4.SeqBitLength
+	if options.SeqBitLength == 0 {
+		seqBitLength = 6
+	} else {
+		seqBitLength = options.SeqBitLength
+	}
+
+	// 5.MaxSeqNumber
+	if options.MaxSeqNumber <= 0 {
+		maxSeqNumber = (1 << seqBitLength) - 1
+	} else {
+		maxSeqNumber = options.MaxSeqNumber
+	}
+
+	// 6.MinSeqNumber
+	var minSeqNumber = options.MinSeqNumber
+
+	// 7.Others
+	var topOverCostCount = options.TopOverCostCount
+	if topOverCostCount == 0 {
+		topOverCostCount = 2000
+	}
+
+	timestampShift := (byte)(workerIdBitLength + seqBitLength)
+	currentSeqNumber := minSeqNumber
 
 	return &SnowWorkerM1{
 		BaseTime:          baseTime,
-		WorkerId:          workerId,
 		WorkerIdBitLength: workerIdBitLength,
+		WorkerId:          workerId,
 		SeqBitLength:      seqBitLength,
 		MaxSeqNumber:      maxSeqNumber,
 		MinSeqNumber:      minSeqNumber,
 		TopOverCostCount:  topOverCostCount,
 		_TimestampShift:   timestampShift,
-		_CurrentSeqNumber: currentSeqNumber}
+		_CurrentSeqNumber: currentSeqNumber,
+
+		_LastTimeTick:           0,
+		_TurnBackTimeTick:       0,
+		_TurnBackIndex:          0,
+		_IsOverCost:             false,
+		_OverCostCountInOneTerm: 0,
+		_GenCountInOneTerm:      0,
+		_TermIndex:              0,
+	}
 }
 
 // DoGenIDAction .
-func (m1 *SnowWorkerM1) DoGenIDAction(arg *OverCostActionArg) {
+func (m1 *SnowWorkerM1) DoGenIdAction(arg *OverCostActionArg) {
 
 }
 
-// BeginOverCostAction .
 func (m1 *SnowWorkerM1) BeginOverCostAction(useTimeTick int64) {
 
 }
 
-// EndOverCostAction .
 func (m1 *SnowWorkerM1) EndOverCostAction(useTimeTick int64) {
 	if m1._TermIndex > 10000 {
 		m1._TermIndex = 0
 	}
 }
 
-// BeginTurnBackAction .
 func (m1 *SnowWorkerM1) BeginTurnBackAction(useTimeTick int64) {
 
 }
 
-// EndTurnBackAction .
 func (m1 *SnowWorkerM1) EndTurnBackAction(useTimeTick int64) {
 
 }
 
-// NextOverCostID .
-func (m1 *SnowWorkerM1) NextOverCostID() uint64 {
+func (m1 *SnowWorkerM1) NextOverCostId() uint64 {
 	currentTimeTick := m1.GetCurrentTimeTick()
 	if currentTimeTick > m1._LastTimeTick {
 		m1.EndOverCostAction(currentTimeTick)
@@ -111,7 +137,7 @@ func (m1 *SnowWorkerM1) NextOverCostID() uint64 {
 		m1._IsOverCost = false
 		m1._OverCostCountInOneTerm = 0
 		m1._GenCountInOneTerm = 0
-		return m1.CalcID(m1._LastTimeTick)
+		return m1.CalcId(m1._LastTimeTick)
 	}
 	if m1._OverCostCountInOneTerm >= m1.TopOverCostCount {
 		m1.EndOverCostAction(currentTimeTick)
@@ -120,7 +146,7 @@ func (m1 *SnowWorkerM1) NextOverCostID() uint64 {
 		m1._IsOverCost = false
 		m1._OverCostCountInOneTerm = 0
 		m1._GenCountInOneTerm = 0
-		return m1.CalcID(m1._LastTimeTick)
+		return m1.CalcId(m1._LastTimeTick)
 	}
 	if m1._CurrentSeqNumber > m1.MaxSeqNumber {
 		m1._LastTimeTick++
@@ -129,15 +155,15 @@ func (m1 *SnowWorkerM1) NextOverCostID() uint64 {
 		m1._OverCostCountInOneTerm++
 		m1._GenCountInOneTerm++
 
-		return m1.CalcID(m1._LastTimeTick)
+		return m1.CalcId(m1._LastTimeTick)
 	}
 
 	m1._GenCountInOneTerm++
-	return m1.CalcID(m1._LastTimeTick)
+	return m1.CalcId(m1._LastTimeTick)
 }
 
 // NextNormalID .
-func (m1 *SnowWorkerM1) NextNormalID() uint64 {
+func (m1 *SnowWorkerM1) NextNormalId() uint64 {
 	currentTimeTick := m1.GetCurrentTimeTick()
 	if currentTimeTick < m1._LastTimeTick {
 		if m1._TurnBackTimeTick < 1 {
@@ -151,19 +177,22 @@ func (m1 *SnowWorkerM1) NextNormalID() uint64 {
 			m1.BeginTurnBackAction(m1._TurnBackTimeTick)
 		}
 
-		time.Sleep(time.Duration(1) * time.Millisecond)
-		return m1.CalcTurnBackID(m1._TurnBackTimeTick)
+		// time.Sleep(time.Duration(1) * time.Millisecond)
+		return m1.CalcTurnBackId(m1._TurnBackTimeTick)
 	}
+
 	// 时间追平时，_TurnBackTimeTick清零
 	if m1._TurnBackTimeTick > 0 {
 		m1.EndTurnBackAction(m1._TurnBackTimeTick)
 		m1._TurnBackTimeTick = 0
 	}
+
 	if currentTimeTick > m1._LastTimeTick {
 		m1._LastTimeTick = currentTimeTick
 		m1._CurrentSeqNumber = m1.MinSeqNumber
-		return m1.CalcID(m1._LastTimeTick)
+		return m1.CalcId(m1._LastTimeTick)
 	}
+
 	if m1._CurrentSeqNumber > m1.MaxSeqNumber {
 		m1.BeginOverCostAction(currentTimeTick)
 		m1._TermIndex++
@@ -173,21 +202,21 @@ func (m1 *SnowWorkerM1) NextNormalID() uint64 {
 		m1._OverCostCountInOneTerm = 1
 		m1._GenCountInOneTerm = 1
 
-		return m1.CalcID(m1._LastTimeTick)
+		return m1.CalcId(m1._LastTimeTick)
 	}
 
-	return m1.CalcID(m1._LastTimeTick)
+	return m1.CalcId(m1._LastTimeTick)
 }
 
 // CalcID .
-func (m1 *SnowWorkerM1) CalcID(useTimeTick int64) uint64 {
+func (m1 *SnowWorkerM1) CalcId(useTimeTick int64) uint64 {
 	result := uint64(useTimeTick<<m1._TimestampShift) + uint64(m1.WorkerId<<m1.SeqBitLength) + uint64(m1._CurrentSeqNumber)
 	m1._CurrentSeqNumber++
 	return result
 }
 
 // CalcTurnBackID .
-func (m1 *SnowWorkerM1) CalcTurnBackID(useTimeTick int64) uint64 {
+func (m1 *SnowWorkerM1) CalcTurnBackId(useTimeTick int64) uint64 {
 	result := uint64(useTimeTick<<m1._TimestampShift) + uint64(m1.WorkerId<<m1.SeqBitLength) + uint64(m1._TurnBackIndex)
 	m1._TurnBackTimeTick--
 	return result
@@ -213,7 +242,8 @@ func (m1 *SnowWorkerM1) NextId() uint64 {
 	m1.Lock()
 	defer m1.Unlock()
 	if m1._IsOverCost {
-		return m1.NextOverCostID()
+		return m1.NextOverCostId()
+	} else {
+		return m1.NextNormalId()
 	}
-	return m1.NextNormalID()
 }
