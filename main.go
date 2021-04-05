c6d811882740c3f426d1b0bfea82739079031e65
zhouzj <zhouzj@zhouzj.com>
2021-04-05 14:49:13 +0800

    auto commit

--- main.go
+++ main.go
@@ -28,6 +28,6 @@ func main() {
 	}
 }
 
-// go build -o ./target/yitidgengo.dll -buildmode=c-shared main.go
-// go install -buildmode=shared -linkshared std
-// go build -o ./target/yitidgengo.so -buildmode=c-shared main.go
+// windows: go build -o ./target/yitidgengo.dll -buildmode=c-shared main.go
+//// go install -buildmode=shared -linkshared std
+//linux: go build -o ./target/yitidgengo.so -buildmode=c-shared main.go
