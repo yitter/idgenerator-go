26e2bf1ed463cd076458b239151f0d4d60ed7fdf
houseme <housemecn@gmail.com>
2022-07-10 09:49:51 +0800

    feat: improve code

--- main.go
+++ main.go
@@ -4,45 +4,43 @@ import (
 	"fmt"
 	"time"
 
-	"C"
-
 	"github.com/yitter/idgenerator-go/idgen"
 	"github.com/yitter/idgenerator-go/regworkerid"
 )
 
-//export SetOptions
+// SetOptions .
 func SetOptions(workerId uint16) {
 	var options = idgen.NewIdGeneratorOptions(workerId)
 	idgen.SetIdGenerator(options)
 }
 
-//export NextId
+// NextId .
 func NextId() int64 {
 	return idgen.NextId()
 }
 
+// RegisterOne .
 // 注册一个 WorkerId，会先注销所有本机已注册的记录
-//export RegisterOne
-func RegisterOne(ip *C.char, port int32, password *C.char, maxWorkerId int32) int32 {
-	return regworkerid.RegisterOne(C.GoString(ip), port, C.GoString(password), maxWorkerId)
+func RegisterOne(ip, password string, port, maxWorkerId int32) int32 {
+	return regworkerid.RegisterOne(ip, port, password, maxWorkerId)
 }
 
+// RegisterMany .
 // 注册多个 WorkerId，会先注销所有本机已注册的记录
-// /export RegisterMany
-func RegisterMany(ip *C.char, port int32, password *C.char, maxWorkerId int32, totalCount int32) []int32 {
+func RegisterMany(ip, password string, port, maxWorkerId int32, totalCount int32) []int32 {
 	// values := regworkerid.RegisterMany(C.GoString(ip), port, C.GoString(password), maxWorkerId, totalCount)
 	// return (*C.int)(unsafe.Pointer(&values))
-	return regworkerid.RegisterMany(C.GoString(ip), port, C.GoString(password), maxWorkerId, totalCount)
+	return regworkerid.RegisterMany(ip, port, password, maxWorkerId, totalCount)
 }
 
+// UnRegister .
 // 注销本机已注册的 WorkerId
-//export UnRegister
 func UnRegister() {
 	regworkerid.UnRegister()
 }
 
+// Validate .
 // 检查本地WorkerId是否有效（0-有效，其它-无效）
-//export Validate
 func Validate(workerId int32) int32 {
 	return regworkerid.Validate(workerId)
 }
